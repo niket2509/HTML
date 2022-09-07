d121f10ca4d3f70f278e6e2adbf8670f5ab526a7
Niket.Goswami <niket.puri@gmail.com>
2022-09-07 15:16:10 +0530

    Initial commit

---
+++ index1.js
@@ -0,0 +1,11 @@
+let screen;
+screen = document.getElementById('screen');
+buttons = document.queryCommandValue('button')
+for(item of buttons){
+  item.addEventListener('click',(e)=>{
+    buttonText=e.target.innerText;
+    console.log('Button Text is',buttonText)
+
+  })
+}
+
