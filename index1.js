bb2a3e5a76beb0b09123cfbb4822ef08ac287df8
Niket.Goswami <niket.puri@gmail.com>
2022-09-13 12:22:53 +0530

    HTML COMMIT

--- index1.js
+++ index1.js
@@ -1,11 +1,12 @@
-let screen;
+let screen, buttonText;
 screen = document.getElementById('screen');
 buttons = document.queryCommandValue('button')
 for(item of buttons){
-  item.addEventListener('click',(e)=>{
-    buttonText=e.target.innerText;
-    console.log('Button Text is',buttonText)
+  item.addEventListener('click',
+    (e) => {
+      buttonText = e.target.innerText;
+      console.log('Button Text is', buttonText)
 
-  })
+    })
 }
 
